b085fdba49ec89ddc018927c10ce3fbb844c1ae9
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-12 16:36:26 +0300

    This is the solution to task 1.1.4
    Pre-project

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,42 @@
 package jm.task.core.jdbc.service;
-
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;                       // JDBC solution
 import jm.task.core.jdbc.model.User;
 
+import javax.transaction.SystemException;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+// слой Service отвечает за бизнес-логику
+// Service на этот раз использует реализацию DAO через Hibernate (ТЗ)
+// Переключаем переменную userDao
 
+public class UserServiceImpl implements UserService {
+
+    //   private final UserDao userDao = new UserDaoJDBCImpl();     // JDBC solution
+    private final UserDao userDao = new UserDaoHibernateImpl();  // Hibernate solution
+
+    public void createUsersTable() throws SystemException {
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable();
     }
 }
